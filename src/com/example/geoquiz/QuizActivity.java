4e0f7324c83a3ad9340fe36096d9e832269caee3
zcranberry <zxdpku@126.com>
2014-11-30 22:46:06 +0800

    first commit

---
+++ src/com/example/geoquiz/QuizActivity.java
@@ -0,0 +1,90 @@
+package com.example.geoquiz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
+
+public class QuizActivity extends ActionBarActivity {
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_asia, true)
+	};
+	
+	private int mCurrentIndex = 0;
+	
+	private void updateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		
+		mQuestionTextView = (TextView)findViewById(R.id.question_test_view);
+		
+		
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		updateQuestion();
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub		
+				Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
+
+	/*@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}*/
+}
